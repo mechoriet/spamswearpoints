108bacb07ca49cd60a868e329fc1bc3d661b8262
mechoriet <kevinworm92@gmail.com>
2015-06-29 21:32:37 +0200

    Updated notepad++ lazzy fixes

--- src/main/java/me/mechoriet/spamswearpointer/objects/pointer.java
+++ src/main/java/me/mechoriet/spamswearpointer/objects/pointer.java
@@ -9,16 +9,33 @@ public class pointer {
     UUID uuid;
     int spam;
     int swear;
-    public pointer(UUID uuid){
+    
+    public pointer(UUID uuid) {
         this.uuid = uuid;
     }
-    public Player getplayer(){
+    public pointer(UUID uuid,int spam,intswear) {
+        this.uuid = uuid;
+        this.spam = spam;
+        this.swear = swear;
+    }
+    public Player getplayer() {
         return Bukkit.getPlayer(this.uuid);
     }
-    public void addspampoint(){
+    
+    public void addspampoint() {
         this.spam = this.spam + 1;
     }
-    public void addswearpoint(){
+    
+    public void resetspampoints() {
+        this.spam = 0;
+    }
+    
+    public void addswearpoint() {
         this.swear = this.swear + 1;
     }
+    
+    public void resetswearpoints() {
+        this.swear = 0;
+    }
+    
 }
